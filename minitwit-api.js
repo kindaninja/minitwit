de79dd258144c6053acaad75b64a76c9b43594a0
emir <emir@itu.dk>
2020-02-17 12:37:10 +0100

    DB helper functions and hash function

--- minitwit-api.js
+++ minitwit-api.js
@@ -19,3 +19,58 @@ let db = new sqlite3.Database('/tmp/minitwit.db', (err) => {
 // Start application
 app.listen(5001);
 console.log('MiniTwit API is running on port 5001..');
+
+//// Database helper functions
+// Select only 1 row
+function selectOne(query, params) {
+    return new Promise((resolve, reject) => {
+        db.get(query, params, (err, row) => {
+            if (err) {
+                console.error(err.message);
+                reject();
+            }
+            resolve(row);
+        });
+    });
+}
+
+// Select multiple rows
+function selectAll(query, params) {
+    return new Promise((resolve, reject) => {
+        db.all(query, params, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject();
+            }
+            resolve(rows);
+        });
+    });
+}
+// Insert 1 row
+function insertOne(query, params) {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, function(err) {
+            if (err) {
+                console.error(err.message);
+                reject();
+            }
+            // Return the last inserted id
+            resolve(this.lastID)
+        });
+    });
+}
+
+// Lame password hash function
+// TODO Use proper hashing library, bcrypt maybe?
+String.prototype.lameHash = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return 'lame' + hash + 'hash';
+}
