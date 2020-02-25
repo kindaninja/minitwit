7f202cf231db165c16e9e7a8882303aaee0bd731
Esben Gade Hellmuth <eshe@itu.dk>
2020-02-25 18:03:38 +0100

    Travis CI/CD

---
+++ travis/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 id_rsa
+ssh-add id_rsa
+git config --global push.default matching
+git remote add deploy ssh://deploy@$IP:$PORT$DEPLOY_DIR
+git push deploy master
+
+ssh deploy@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  echo deploy | sudo -S docker-compose up -d --build
+EOF
