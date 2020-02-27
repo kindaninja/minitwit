829c6eaf711b6dd436b93148fe3a9c80e4e71f1f
rdmo <rdmo@itu.dk>
2020-02-27 15:27:52 +0100

    Jesus Christ# Please enter the commit message for your changes. Lines starting

--- app/persistence/sqlite.js
+++ app/persistence/sqlite.js
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 
-let db = new sqlite3.Database('persistence/minitwit.db', (err) => {
+let db = new sqlite3.Database('/tmp/minitwit.db', (err) => {
     if (err) {
         return console.error(err.message);
     }
