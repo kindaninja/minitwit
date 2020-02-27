511407e77c59b31286286b92e4614eb3ef505784
Esben Gade Hellmuth <eshe@itu.dk>
2020-02-27 13:11:25 +0100

    persistence + more app

--- app/app.js
+++ app/app.js
@@ -1,6 +1,23 @@
 // Express web framework https://expressjs.com/
 const express = require('express');
-const app = express();
 // Sessions https://github.com/expressjs/session#readme
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const SECRET = 'shhhhhhhhhh';
+
+// === Web app ===
+const web = express();
+// EJS template engine https://ejs.co/
+web.set('view engine', 'ejs');
+// Static files
+web.use(express.static('static'));
+// Form data
+web.use(express.urlencoded())
+// Sessions
+web.use(cookieParser());
+web.use(session({secret: SECRET}));
+
+// === Web API ===
+const api = express();
