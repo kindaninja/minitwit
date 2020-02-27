b220b2bed9c84a28fa8dc1e3c80996f778a5b1aa
Esben Gade Hellmuth <eshe@itu.dk>
2020-02-27 13:44:47 +0100

    Ready for db abstraction layer

--- app/app.js
+++ app/app.js
@@ -3,11 +3,14 @@ const express = require('express');
 // Sessions https://github.com/expressjs/session#readme
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const bodyParser = require('body-parser');
+
+const webRouter = require('./routers/web_router');
+const apiRouter = require('./routers/api_router');
 
 const SECRET = 'shhhhhhhhhh';
 
 // === Web app ===
+
 const web = express();
 // EJS template engine https://ejs.co/
 web.set('view engine', 'ejs');
@@ -19,5 +22,13 @@ web.use(express.urlencoded())
 web.use(cookieParser());
 web.use(session({secret: SECRET}));
 
+web.use('/', webRouter);
+web.listen(8080, () => console.log('Minitwit web app listening on port 8080.'));
+
 // === Web API ===
+
 const api = express();
+api.use(express.json());
+
+api.use('/', apiRouter)
+api.listen(9090, () => console.log('Minitwit API listening on port 9090.'));
---
+++ app/routers/api_router.js
@@ -0,0 +1,228 @@
+const express = require('express');
+const router = express.Router();
+
+let LATEST = 0;
+
+// Get latest value
+router.get('/latest', function (req, res) {
+    return res.json({"latest": LATEST});
+});
+
+// Register handler
+router.post('/register', async function(req, res) {
+    updateLatest(req);
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.pwd;
+    console.log(username, email, password);
+    let error;
+    if(!username) {
+        error = 'You have to enter a username';
+    } else if (!email || !email.includes('@')) {
+        error = 'You have to enter a valid email address';
+    } else if (!password) {
+        error = 'You have to enter a password';
+    } else if (await selectOne('SELECT 1 FROM user WHERE username = ?',[username])) {
+        error = 'The username is already taken';
+    } else {
+        await insertOne(
+            'INSERT INTO user(username, email, pw_hash) VALUES(?, ?, ?)',
+            [username, email, password.lameHash()]);
+        return res.status(204).send();
+
+    }
+    return res.json({"status": 400, "error_msg": error}).status(400).send();
+});
+
+// Public timeline page
+router.get('/msgs', async function(req, res) {
+    updateLatest(req);
+    let not_from_sim = notReqFromSimulator(req);
+    if (not_from_sim) {
+        let error = "You are not authorized to use this resource!";
+        return res.json({"status": 403, "error_msg": error}).status(403).send();
+    }
+
+    let no_msgs = req.query.no ? req.query.no : 100;
+    let messages = await selectAll(
+        `select message.*, user.* from message, user
+                where message.author_id = user.user_id
+                order by message.pub_date desc limit ?`,
+        [no_msgs]
+    );
+
+    let filteredMsgs = [];
+    messages.forEach(function(msg) {
+        let filteredMsg = {};
+        filteredMsg["content"] = msg["text"];
+        filteredMsg["pub_date"] = msg["pub_date"];
+        filteredMsg["user"] = msg["username"];
+        filteredMsgs.push(filteredMsg);
+    });
+    return res.json(filteredMsgs);
+});
+
+router.get('/msgs/:username', async function(req, res) {
+    updateLatest(req);
+
+    let not_from_sim = notReqFromSimulator(req);
+    if (not_from_sim) {
+        let error = "You are not authorized to use this resource!";
+        return res.json({"status": 403, "error_msg": error}).status(403).send();
+    }
+    const { username } = req.params;
+    let no_msgs = req.query.no ? req.query.no : 100;
+
+    let profile_user = await selectOne(
+        'SELECT * FROM user WHERE username = ?',
+        [username]);
+
+    if(!profile_user) {
+        return res.status(404).send();
+    }
+
+    let messages = await selectAll(
+        `SELECT message.*, user.* FROM message, user WHERE
+                user.user_id = message.author_id AND user.user_id = ?
+                ORDER BY message.pub_date DESC LIMIT ?`,
+        [profile_user.user_id, no_msgs]);
+
+    let filteredMsgs = [];
+    messages.forEach(function(msg) {
+        let filteredMsg = {};
+        filteredMsg["content"] = msg["text"];
+        filteredMsg["pub_date"] = msg["pub_date"];
+        filteredMsg["user"] = msg["username"];
+        filteredMsgs.push(filteredMsg);
+    });
+
+    return res.json(filteredMsgs);
+});
+
+router.post('/msgs/:username', async function (req, res) {
+    updateLatest(req);
+
+    let not_from_sim = notReqFromSimulator(req);
+    if (not_from_sim) {
+        let error = "You are not authorized to use this resource!";
+        return res.json({"status": 403, "error_msg": error}).status(403).send();
+    }
+    const { username } = req.params;
+    let profile_user = await selectOne(
+        'SELECT * FROM user WHERE username = ?',
+        [username]);
+
+    if(!profile_user) {
+        return res.status(404).send();
+    }
+
+    let content = req.body.content;
+
+    await insertOne(
+        'INSERT INTO message(author_id, text, pub_date) VALUES(?, ?, ?)',
+        [profile_user.user_id, content, Date.now()]);
+
+    return res.status(204).send();
+});
+
+router.get('/fllws/:username', async function (req, res) {
+    updateLatest(req);
+
+    let not_from_sim = notReqFromSimulator(req);
+    if (not_from_sim) {
+        let error = "You are not authorized to use this resource!";
+        return res.json({"status": 403, "error_msg": error}).status(403).send();
+    }
+    const { username } = req.params;
+
+    let profile_user = await selectOne(
+        'SELECT * FROM user WHERE username = ?',
+        [username]);
+
+    if(!profile_user) {
+        return res.status(404).send();
+    }
+
+    let no_followers = req.query.no ? req.query.no : 100;
+
+    let followers = await selectAll(
+        `SELECT user.username FROM user
+                INNER JOIN follower ON follower.whom_id = user.user_id
+                WHERE follower.who_id = ?
+                LIMIT ?`,
+        [profile_user.user_id, no_followers]
+    );
+    let followers_names = [];
+    followers.forEach((follower) => {
+        followers_names.push(follower["username"])
+    });
+
+    return res.json({"follows": followers_names});
+});
+
+router.post('/fllws/:username', async function (req, res) {
+    updateLatest(req);
+
+    let not_from_sim = notReqFromSimulator(req);
+    if (not_from_sim) {
+        let error = "You are not authorized to use this resource!";
+        return res.json({"status": 403, "error_msg": error}).status(403).send();
+    }
+    const { username } = req.params;
+
+    let profile_user = await selectOne(
+        'SELECT * FROM user WHERE username = ?',
+        [username]);
+
+    if(!profile_user) {
+        return res.status(404).send();
+    }
+
+    if (req.body.follow) {
+        let follow_username = req.body.follow;
+        let follows_user = await selectOne(
+            'SELECT * FROM user WHERE username = ?',
+            [follow_username]);
+
+        if (!follows_user) {
+            return res.status(404).send();
+        }
+
+        await insertOne(
+            'INSERT INTO follower (who_id, whom_id) VALUES (?, ?)',
+            [profile_user.user_id, follows_user.user_id]);
+
+        return res.status(204).send();
+    }
+
+    if (req.body.unfollow) {
+        let unfollow_username = req.body.unfollow;
+        let unfollows_user = await selectOne(
+            'SELECT * FROM user WHERE username = ?',
+            [unfollow_username]);
+
+        if (!unfollows_user){
+            return res.status(404).send();
+        }
+
+        await deleteRows(
+            'DELETE FROM follower WHERE who_id = ? AND whom_id = ?',
+            [profile_user.user_id, unfollows_user.user_id]);
+
+        return res.status(204).send();
+    }
+});
+
+// Checks if simulator
+function notReqFromSimulator(request) {
+    let from_sim = request.headers.authorization;
+    return from_sim !== "Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh";
+}
+
+// Update latest
+function updateLatest(request) {
+    let try_latest = request.query.latest ? request.query.latest : -1;
+    LATEST = try_latest !== -1 ? parseInt(try_latest) : LATEST;
+}
+
+module.exports = router;
