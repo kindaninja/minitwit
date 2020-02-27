35766fc6f6268d9709583927cdca38a86c74c98a
emir <emir@itu.dk>
2020-02-27 14:27:28 +0100

    Removed helper functions from minitwit.js and minitwit-api.js

--- webapi/minitwit-api.js
+++ webapi/minitwit-api.js
@@ -9,15 +9,6 @@ let LATEST = 0;
 
 app.use(bodyParser.json());
 
-// SQLite3 https://github.com/mapbox/node-sqlite3
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('/tmp/minitwit.db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the minitwit.db SQlite database.');
-});
-
 // Get latest value
 app.get('/latest', function (req, res) {
    return res.json({"latest": LATEST});
@@ -233,60 +224,6 @@ app.post('/fllws/:username', async function (req, res) {
 app.listen(5001);
 console.log('MiniTwit API is running on port 5001..');
 
-// //// Database helper functions
-// // Select only 1 row
-// function selectOne(query, params) {
-//     return new Promise((resolve, reject) => {
-//         db.get(query, params, (err, row) => {
-//             if (err) {
-//                 console.error(err.message);
-//                 reject();
-//             }
-//             resolve(row);
-//         });
-//     });
-// }
-//
-// // Select multiple rows
-// function selectAll(query, params) {
-//     return new Promise((resolve, reject) => {
-//         db.all(query, params, (err, rows) => {
-//             if (err) {
-//                 console.error(err.message);
-//                 reject();
-//             }
-//             resolve(rows);
-//         });
-//     });
-// }
-// // Insert 1 row
-// function insertOne(query, params) {
-//     return new Promise((resolve, reject) => {
-//         db.run(query, params, function(err) {
-//             if (err) {
-//                 console.error(err.message);
-//                 reject();
-//             }
-//             // Return the last inserted id
-//             resolve(this.lastID);
-//         });
-//     });
-// }
-//
-// // Delete row(s)
-// function deleteRows(query, params) {
-//     return new Promise((resolve, reject) => {
-//         db.run(query, params, function(err) {
-//             if (err) {
-//                 console.error(err.message);
-//                 reject();
-//             }
-//             // Return the deleted
-//             resolve(this.changes);
-//         });
-//     });
-// }
-
 // Checks if simulator
 function notReqFromSimulator(request) {
     let from_sim = request.headers.authorization;
@@ -298,18 +235,3 @@ function updateLatest(request) {
     let try_latest = request.query.latest ? request.query.latest : -1;
     LATEST = try_latest !== -1 ? parseInt(try_latest) : LATEST;
 }
-
-// // Lame password hash function
-// // TODO Use proper hashing library, bcrypt maybe?
-// String.prototype.lameHash = function() {
-//     var hash = 0;
-//     if (this.length === 0) {
-//         return hash;
-//     }
-//     for (var i = 0; i < this.length; i++) {
-//         var char = this.charCodeAt(i);
-//         hash = ((hash<<5)-hash)+char;
-//         hash = hash & hash; // Convert to 32bit integer
-//     }
-//     return 'lame' + hash + 'hash';
-// }
